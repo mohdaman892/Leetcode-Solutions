ef1f11672188cb70392026f8b60e89b59aabd564
Mohd Aman <38840807+mohdaman892@users.noreply.github.com>
2025-02-09 01:34:29 +0530

    Time: 9104 ms (5.26%) | Memory: 151.9 MB (5.74%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.py
@@ -0,0 +1,32 @@
+class NumberContainers:
+
+    def __init__(self):
+        self.ind_to_num = {}
+        self.num_to_inds = {}
+        
+
+    def change(self, index: int, number: int) -> None:
+        from sortedcontainers import SortedSet
+        prev = None
+        if index in self.ind_to_num:
+            prev = self.ind_to_num[index]
+        self.ind_to_num[index] = number
+        if prev:
+            self.num_to_inds[prev].discard(index)
+            if not self.num_to_inds[prev]:
+                self.num_to_inds.pop(prev)
+        if number not in self.num_to_inds:
+            self.num_to_inds[number] = SortedSet()
+        self.num_to_inds[number].add(index)
+        
+
+    def find(self, number: int) -> int:
+        if number not in self.num_to_inds:
+            return -1
+        return list(self.num_to_inds[number])[0]
+
+
+# Your NumberContainers object will be instantiated and called as such:
+# obj = NumberContainers()
+# obj.change(index,number)
+# param_2 = obj.find(number)
