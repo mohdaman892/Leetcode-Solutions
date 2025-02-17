f0708ccd26a42f36a05f28bd125ad05a3eb65624
Mohd Aman <38840807+mohdaman892@users.noreply.github.com>
2025-02-17 14:27:44 +0530

    Time: 27 ms (76.29%) | Memory: 19.7 MB (25.88%) - LeetSync

---
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+
+        def f(i,c,a,n):
+            if len(a) == c:
+                s.add("".join(x for x in list(a)))
+                return
+            if i == n:
+                return
+            a.append(tiles[i])
+            f(i+1,c,a,n)
+            a.pop()
+            f(i+1,c,a,n)
+
+
+        n = len(tiles)
+        ans = 0
+        for c in range(1,n+1):
+            s = set()
+            f(0,c,[],n)
+            print(s)
+            s2 = set()
+            for st in s:
+                for st2 in set(permutations(st)):
+                    s2.add(st2)
+            ans += len(s2)
+
+        
+        return ans
