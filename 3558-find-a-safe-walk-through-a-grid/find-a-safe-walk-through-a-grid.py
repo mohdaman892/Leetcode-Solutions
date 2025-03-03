e4cc4e8cf5c72e644119bd541617b81bd8f693cd
Mohd Aman <38840807+mohdaman892@users.noreply.github.com>
2025-03-04 01:02:04 +0530

    Time: 159 ms (57.25%) | Memory: 18.3 MB (69.91%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
@@ -1,36 +1,30 @@
 class Solution:
     def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
-        
-        @lru_cache(None)
-        def f(i,j,h):
-            if h==0:
-                return False
-            if i==n-1 and j==m-1:
-                return h>=1
-            
-
-            dx = [0,1,0,-1]
-            dy = [1,0,-1,0]
-            ans = False
-            for x in range(len(dx)):
-                nx = i+dx[x]
-                ny = j+dy[x]
-                if nx>=0 and nx<n and ny>=0 and ny<m and not vis[nx][ny]:
-                    vis[nx][ny] = 1
-                    if grid[nx][ny] == 1:
-                        ans |= f(nx,ny,h-1)
-                    else:
-                        ans |= f(nx,ny,h)
-                    vis[nx][ny] = 0
-            return ans
-
-
-
 
         n = len(grid)
         m = len(grid[0])
         vis = [[0 for i in range(m)] for j in range(n)]
         vis[0][0] = 1
+        dist = [[sys.maxsize for i in range(m)] for j in range(n)]
         if grid[0][0] == 1:
-            health-=1
-        return f(0,0,health)
+            dist[0][0] = 1
+        else:
+            dist[0][0] = 0
+        pq = [[0,0,dist[0][0]]]
+        heapify(pq)
+        while pq:
+            r = heappop(pq)
+            x,y = r[0],r[1]
+            dx = [0,1,0,-1]
+            dy = [1,0,-1,0]
+            for i in range(len(dx)):
+                nx = x+dx[i]
+                ny = y+dy[i]
+                if nx>=0 and ny>=0 and nx<n and ny<m:
+                    d = 0
+                    if grid[nx][ny]==1:
+                        d = 1
+                    if r[2]+d<dist[nx][ny]:
+                        dist[nx][ny] = r[2]+d
+                        heappush(pq,[nx,ny,r[2]+d])
+        return health-dist[n-1][m-1]>=1
