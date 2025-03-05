68d7adbaeef0678a74af752edf60b68aa2f9161d
Mohd Aman <38840807+mohdaman892@users.noreply.github.com>
2025-03-05 10:58:48 +0530

    Time: 421 ms (5.49%) | Memory: 253.3 MB (18.43%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        
+        @lru_cache(None)
+        def f(i,s):
+            if i == len(a):
+                # print(s)
+                return s == n
+            
+            return f(i+1,s+a[i]) or f(i+1,s)
+        
+        
+        a = [1]
+        x = 1
+        while 3**x<=n:
+            a.append(3**x)
+            x+=1
+        print(a)
+        return f(0,0)
