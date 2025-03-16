fec7d506eb90a737525950cda8f0d14bd04ae7db
Mohd Aman <38840807+mohdaman892@users.noreply.github.com>
2025-03-16 16:48:51 +0530

    Time: 2286 ms (33.33%) | Memory: 316.3 MB (0.00%) - LeetSync

---
+++ 3795-zero-array-transformation-iv/zero-array-transformation-iv.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        
+        @lru_cache(None)
+        def f(i,s,ind):
+            if s == 0:
+                return i-1
+            if s < 0:
+                return sys.maxsize
+            if i == len(queries):
+                return i-1 if s == 0 else sys.maxsize
+            ans = sys.maxsize
+            if queries[i][0]<=ind<=queries[i][1]:
+                ans = min(ans,f(i+1,s-queries[i][2],ind))
+            ans = min(ans,f(i+1,s,ind))
+            return ans
+        
+        res = -sys.maxsize
+        for i in range(len(nums)):
+            target = nums[i]
+            x = f(0,target,i)
+            print(x)
+            if x == sys.maxsize:
+                return -1
+            res = max(res,x)
+    
+        return res+1
