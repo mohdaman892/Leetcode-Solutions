92886779aecb739828c53b8d4cb93b10f1201737
Mohd Aman <38840807+mohdaman892@users.noreply.github.com>
2025-03-19 12:55:14 +0530

    Time: 264 ms (5.14%) | Memory: 21.7 MB (54.50%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -17,15 +17,6 @@ class Solution:
                 ans += 1
             else:
                 i+=1
-        # print(nums,i)
-        # if nums[i] == 0:
-        #     for j in range(i,min(i+3,n)):
-        #         if nums[j] == 0:
-        #             nums[j] = 1
-        #         else:
-        #             nums[j] = 0
-        #     ans += 1
-        # print(nums)
         if sum(nums) == len(nums):
             return ans
         else:
