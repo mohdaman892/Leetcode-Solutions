d4c6852cf5b68987b1729de1e80cfb6afea8222c
Mohd Aman <38840807+mohdaman892@users.noreply.github.com>
2025-05-05 13:20:01 +0530

    Time: 11 ms (12.44%) | Memory: 19.7 MB (8.27%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -1,8 +1,8 @@
 class Solution:
     def numTilings(self, n: int) -> int:
 
-        @lru_cache(None)
         def f(i,gap):
+            # print(dp)
             if i>n:
                 return 0
             
@@ -11,11 +11,19 @@ class Solution:
                     return 1
                 else:
                     return 0
+            
+            if dp[i][gap]!=-1:
+                return dp[i][gap]
 
+            ans = 0
             if not gap:
-                return f(i+1,False) + f(i+2,False) + 2*f(i+2,True)
+                ans +=  f(i+1,False) + f(i+2,False) + 2*f(i+2,True)
             else:
-                return f(i+1,False) + f(i+1,True)
+                ans +=  f(i+1,False) + f(i+1,True)
+            dp[i][gap] = ans
+            return ans
 
         mod = 10**9+7
+        dp = [[-1,-1] for i in range(n)]
+        print(dp)
         return f(0,False)%mod
