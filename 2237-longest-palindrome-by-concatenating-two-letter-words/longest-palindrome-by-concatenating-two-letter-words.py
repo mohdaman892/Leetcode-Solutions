e58a76ab581612e7a6d67bdb0834420fc8bb4787
Mohd Aman <mohdaman892@gmail.com>
2025-05-25 13:03:15 +0530

    Time: 40 ms (93.68%) | Memory: 35.8 MB (12.27%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,35 +1,27 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
-        s=set()
-        c=Counter(words)
-        k=0
-        flag=0
-        for i in range(len(words)):
-            if words[i] in s:
-                continue
-            
-            elif words[i][0]==words[i][1] and c[words[i]]>1:
-                if c[words[i]]%2!=0 and flag==0:
-                    flag=1
-                    k+=2*c[words[i]]
-                    s.add(words[i])
-                elif c[words[i]]%2!=0 and flag==1:
-                    k+=2*c[words[i]]-2
-                    s.add(words[i])
-                elif c[words[i]]%2==0:
-                    k+=2*c[words[i]]
-                    s.add(words[i])
-            elif words[i][0]==words[i][1] and c[words[i]]==1 and flag==0:
-                k+=2
-                flag=1
-                s.add(words[i])
-            elif words[i][1]+words[i][0] in c and words[i][0]!=words[i][1]:
-                k+=4*min(c[words[i]],c[words[i][1]+words[i][0]])
-                s.add(words[i])
-                s.add(words[i][1]+words[i][0])
-        print(s)
-        return k
-            
-                
-                
+        c = Counter(words)
+        ans = 0
+        print(c)
+        double_flag = True
+        processed = set()
+        for word in c.keys():
+            if word[0]==word[1]:
+                if c[word]==1:
+                    if double_flag:
+                        ans += 2
+                        double_flag = False
+                else:
+                    ans += 2*(2*(c[word]//2))
+                    if double_flag and c[word]%2==1:
+                        ans += 2
+                        double_flag = False
+            else:
+                rev = word[1]+word[0]
+                if rev in c and rev not in processed and word not in processed:
+                    ans += 4*min(c[word],c[rev])
+                    processed.add(word)
+                    processed.add(rev)
+        
+        return ans
         
